6ca8e9ff2a5c10b8474762009ad59cb1d1050689
Jefferson Carvalho <jeffersoncarvalho60@outlook.com>
2018-08-24 15:43:57 -0300

    Adicionando função de inserção

--- conexaoBancoPDO/PDO.php
+++ conexaoBancoPDO/PDO.php
@@ -1,4 +1,9 @@
 <?php 
+
+	consulta(); //chamando a função consulta
+
+	inserir(); //chamando a função inserir
+
 	function consulta(){ //Função que conecta com o banco e consulta um usuario na tabela
 
 		$dsn = "mysql:dbname=teste;host=localhost"; 
@@ -30,5 +35,30 @@
 			echo "Erro: ".$e->getMessage();
 		}
 	}
+
+	function inserir(){ //Função que insere usuarios no banco teste
+
+		$dsn = "mysql:dbname=teste;host=localhost";
+		$dbuser = "root";
+		$dbpass = "";
+
+		try {
+		$pdo = new PDO($dsn, $dbuser, $dbpass);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$nome = "Fulano";
+		$email = "Fulano@hotmail.com";
+		$senha = md5("123456");
+
+		$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'";
+		$pdo->query($sql); //Inserção, atualização etc não retornam dados, então é só fazer a query
+
+		echo "Usuario inserido com sucesso!!! Id número: ".$pdo->lastInsertId(); //lastInsertId retorna o último id inserido na tabela
+		
+		} catch (PDOException $e) {
+			echo "Erro: ".$e->getMessage();
+		}
+}
+
 
  ?>
