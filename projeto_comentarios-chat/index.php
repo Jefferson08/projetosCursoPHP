9202aee352413b562952aa37d9fbcfac3e362604
Jefferson Carvalho <jeffersoncarvalho60@outlook.com>
2018-08-26 22:20:17 -0300

    Criando formulário de envio

---
+++ projeto_comentarios-chat/index.php
@@ -0,0 +1,11 @@
+<fieldset>
+	<form method="POST">
+		Nome: <br>
+		<input type="text" name="nome"><br><br>
+		Mensagem: <br>
+		<textarea name="mensagem" style="width: 200px; height: 100px;" maxlength="156"></textarea><br><br>
+		<input type="submit" value="Enviar">
+	</form>
+</fieldset>
+
+<br><br>
